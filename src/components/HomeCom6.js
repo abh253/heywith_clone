3ddc726cbacebe92f7934583303e2a8e65947903
abh253 <76785626+abh253@users.noreply.github.com>
2021-11-25 21:25:18 +0530

    Update HomeCom6.js

--- src/components/HomeCom6.js
+++ src/components/HomeCom6.js
@@ -2,11 +2,9 @@ import React from 'react'
 
 const HomeCom6 = () => {
     return (
-       <section className='home-com-6'>
-            <article>
-                <h2>Ready to generate revenue through social media?</h2>
-            </article>
-        </section>
+        <div>
+            hello from home com 6
+        </div>
     )
 }
 
