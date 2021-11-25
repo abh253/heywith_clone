c522a204eff9fbf27099836ce1f26b7b2e838259
abh253 <abhisheksingh2532001@gmail.com>
2021-11-25 21:05:30 +0530

    update

--- src/components/HomeCom6.js
+++ src/components/HomeCom6.js
@@ -1,10 +1,15 @@
 import React from 'react'
-
+import profilesImg from '../data/images/profiles.png'
 const HomeCom6 = () => {
     return (
         <section className='home-com-6'>
-            <article>
+            <article className='home-com-6-text'>
                 <h2>Ready to generate revenue through social media?</h2>
+                <p>Create an account to get early access to use HeyWith</p>
+                <button type='button'>Create your profile</button>
+                <button type='button'>Demo profile</button>
+            </article>
+            <article className='home-com-6-img'>
             </article>
         </section>
     )
